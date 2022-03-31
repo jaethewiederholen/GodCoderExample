326cd003a7e13f17562c6e2265496dbbed7c5ace
JAE YEON KIM <65452447+jaethewiederholen@users.noreply.github.com>
2022-03-31 16:56:15 +0900

    init

---
+++ src/main/java/com/godcoder/GodcoderExample/GodcoderExampleApplication.java
@@ -0,0 +1,13 @@
+package com.godcoder.GodcoderExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodcoderExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodcoderExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/godcoder/GodcoderExample/GodcoderExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godcoder.GodcoderExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodcoderExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
